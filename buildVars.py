4bc9eb09db13387fbf74ee4d2415c6417a68b476
Woody Tseng <tsengwoody.tw@gmail.com>
2025-06-12 15:37:03 +0800

    chore(manifest): update lastTested NVDA version

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Allows access math content written by MathML ; Allows write math content by LaTeX"""),
 	# version
-	"addon_version": "4.2.3",
+	"addon_version": "4.3",
 	# Author(s)
 	"addon_author": "Tseng Woody <tsengwoody.tw@gmail.com>",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2024.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.4",
+	"addon_lastTestedNVDAVersion": "2025.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
