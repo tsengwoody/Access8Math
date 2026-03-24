1645134d198fd6c3133da1b9c42bc23fcbe614f9
Woody Tseng <tsengwoody.tw@gmail.com>
2026-03-24 11:50:01 +0800

    chore: sync web template

--- buildVars.py
+++ buildVars.py
@@ -1,52 +1,53 @@
-# -*- coding: UTF-8 -*-
-
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
+from site_scons.site_tools.NVDATool.typings import AddonInfo, BrailleTables, SymbolDictionaries
 
 # Since some strings in `addon_info` are translatable,
 # we need to include them in the .po files.
 # Gettext recognizes only strings given as parameters to the `_` function.
-# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# To avoid initializing translations in this module we simply import a "fake" `_` function
 # which returns whatever is given to it as an argument.
-def _(arg):
-	return arg
+from site_scons.site_tools.NVDATool.utils import _
 
 
 # Add-on information variables
-addon_info = {
+addon_info = AddonInfo(
 	# add-on Name/identifier, internal for NVDA
-	"addon_name" : "Access8Math",
-	# Add-on summary, usually the user visible name of the addon.
-	# Translators: Summary for this add-on
-	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary" : _("Access8Math"),
+	addon_name="Access8Math",
+	# Add-on summary/title, usually the user visible name of the add-on
+	# Translators: Summary/title for this add-on
+	# to be shown on installation and add-on information found in add-on store
+	addon_summary=_("Access8Math"),
 	# Add-on description
-	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""Allows access math content written by MathML ; Allows write math content by LaTeX"""),
+	# Translators: Long description to be shown for this add-on on add-on information from add-on store
+	addon_description=_("""Allows access math content written by MathML ; Allows write math content by LaTeX"""),
 	# version
-	"addon_version": "4.5",
+	addon_version="4.5",
+	# Brief changelog for this version
+	# Translators: what's new content for the add-on version to be shown in the add-on store
+	addon_changelog=_("Changelog for the add-on version."),
 	# Author(s)
-	"addon_author": "Tseng Woody <tsengwoody.tw@gmail.com>",
+	addon_author="Tseng Woody <tsengwoody.tw@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://addons.nvda-project.org/addons/access8math.en.html",
+	addon_url="https://github.com/tsengwoody/Access8Math",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": "https://github.com/tsengwoody/Access8Math",
+	addon_sourceURL="https://github.com/tsengwoody/Access8Math",
 	# Documentation file name
-	"addon_docFileName": "readme.html",
-	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2024.1",
-	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2025.1",
+	addon_docFileName="readme.html",
+	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
+	addon_minimumNVDAVersion="2024.1",
+	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
+	addon_lastTestedNVDAVersion="2025.3.3",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
+	addon_updateChannel=None,
 	# Add-on license such as GPL 2
-	"addon_license": "GPL v2",
+	addon_license="GPL v2",
 	# URL for the license document the ad-on is licensed under
-	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
-}
+	addon_licenseURL="https://www.gnu.org/licenses/gpl-2.0.html",
+)
 
 # Define the python files that are the sources of your add-on.
 # You can either list every file (using ""/") as a path separator,
@@ -56,30 +57,33 @@ addon_info = {
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = [
+pythonSources: list[str] = [
 	'addon/globalPlugins/Access8Math/*.py',
 	'addon/globalPlugins/Access8Math/command/*.py',
 	'addon/globalPlugins/Access8Math/lib/latex/*.py',
 ]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py"]
+i18nSources: list[str] = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
-excludedFiles = []
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+excludedFiles: list[str] = []
 
 # Base language for the NVDA add-on
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
-baseLanguage = "en"
+# You must also edit .gitignore file to specify base language files to be ignored.
+baseLanguage: str = "en"
 
 # Markdown extensions for add-on documentation
 # Most add-ons do not require additional Markdown extensions.
 # If you need to add support for markup such as tables, fill out the below list.
 # Extensions string must be of the form "markdown.extensions.extensionName"
 # e.g. "markdown.extensions.tables" to add tables.
-markdownExtensions = ['markdown.extensions.tables']
+markdownExtensions: list[str] = ['markdown.extensions.tables']
 
 # Custom braille translation tables
 # If your add-on includes custom braille tables (most will not), fill out this dictionary.
@@ -89,4 +93,13 @@ markdownExtensions = ['markdown.extensions.tables']
 # contracted (contracted (True) or uncontracted (False) braille code),
 # output (shown in output table list),
 # input (shown in input table list).
-brailleTables = {}
+brailleTables: BrailleTables = {}
+
+# Custom speech symbol dictionaries
+# Symbol dictionary files reside in the locale folder, e.g. `locale\en`, and are named `symbols-<name>.dic`.
+# If your add-on includes custom speech symbol dictionaries (most will not), fill out this dictionary.
+# Each key is the name of the dictionary,
+# with keys inside recording the following attributes:
+# displayName (name of the speech dictionary shown to users and translatable),
+# mandatory (True when always enabled, False when not.
+symbolDictionaries: SymbolDictionaries = {}
